d6a170e4909e314bd620679ee0b4b406267cfd15
zhrucong@gmail.com <1QM9pz3al2>
2017-06-23 16:52:15 +0800

    add Bridge

---
+++ src/main/java/Bridge/Circle.java
@@ -0,0 +1,23 @@
+package Bridge;
+
+/**
+ * Description:
+ * User: wind
+ * Date: 2017-06-23
+ * Time: 13:52 下午
+ */
+public class Circle extends Shape {
+    private int x,y,radius;
+
+    public Circle(int x,int y,int radius,DrawAPI drawAPI) {
+        super(drawAPI);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+
+    }
+
+    public void draw() {
+        drawAPI.drawCirle(radius,x,y);
+    }
+}
