3ece9638cb500333ce14344815e45451adaff54f
zhrucong@gmail.com <1QM9pz3al2>
2017-08-11 10:58:27 +0800

    add Decorator

---
+++ src/main/java/Decorator/Rectangle.java
@@ -0,0 +1,13 @@
+package Decorator;
+
+/**
+ * Description:
+ * User: wind
+ * Date: 2017-06-30
+ * Time: 14:56 下午
+ */
+public class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Shape:Rectangle");
+    }
+}
