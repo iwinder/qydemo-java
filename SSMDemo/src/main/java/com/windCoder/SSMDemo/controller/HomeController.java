4360a05c6776fa51448c9ecfaa7d18888212458f
zhrucong@gmail.com <1QM9pz3al2>
2017-08-17 23:57:55 +0800

    添加基于AdminLTE的前端页面并实现局部刷新与动态链接

--- SSMDemo/src/main/java/com/windCoder/SSMDemo/controller/HomeController.java
+++ SSMDemo/src/main/java/com/windCoder/SSMDemo/controller/HomeController.java
@@ -21,6 +21,39 @@ public class HomeController {
         return "home";
     }
 
+    @RequestMapping("admin/login.do")
+    public String login(HttpServletRequest request) {
+
+        return "admin/login";
+    }
+
+    @RequestMapping("admin/home.do")
+    public String adminHome(HttpServletRequest request) {
+
+        return "admin/home";
+    }
+
+    @RequestMapping("admin/user/home.do")
+    public String adminUserHome(HttpServletRequest request) {
+
+        return "admin/user/home";
+    }
+    @RequestMapping("admin/user/add.do")
+    public String adminUserAdd(HttpServletRequest request) {
+
+        return "admin/user/add";
+    }
+    @RequestMapping("admin/post/home.do")
+    public String adminPostHome(HttpServletRequest request) {
+
+        return "admin/post/home";
+    }
+    @RequestMapping("admin/post/add.do")
+    public String adminPostAdd(HttpServletRequest request) {
+
+        return "admin/post/add";
+    }
+
 
 
 }
---
+++ SSMDemo/src/main/webapp/WEB-INF/jsp/admin/user/home.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                用户概览
+                <small>Control panel</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">用户概览</li>
+            </ol>
+        </section>
+
