0113de901926bc82bfcc5e5436bb1a2af9c0cf4b
zhangrucong <zhangrucong@parim.net>
2018-03-21 14:00:20 +0800

    添加线程相关测试

--- src/main/java/Others/base/ThreadLocal/SimpleThreadLocal.java
+++ src/main/java/Others/base/ThreadLocal/SimpleThreadLocal.java
@@ -1,4 +1,34 @@
 package Others.base.ThreadLocal;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SimpleThreadLocal {
+    private Map valueMap = Collections.synchronizedMap(new HashMap());
+
+    public void set (Object newValue){
+        //①键为线程对象，值为本线程的变量副本
+        valueMap.put(Thread.currentThread(),newValue);
+    }
+
+    public Object get(){
+        Thread currentThread = Thread.currentThread();
+        //②返回本线程对应的变量
+        Object o = valueMap.get(currentThread);
+        //③如果在Map中不存在，放到Map中保存起来
+        if (o == null && !valueMap.containsKey(currentThread)){
+            o = initiaValue();
+            valueMap.put(currentThread,o);
+        }
+        return o;
+    }
+
+    public void remove(){
+        valueMap.remove(Thread.currentThread());
+    }
+
+    public Object initiaValue(){
+        return null;
+    }
 }
