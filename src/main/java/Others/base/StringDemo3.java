c08641e872475b1f8ae91232d3934bbcfee2e5a1
zhangrucong <zhangrucong@parim.net>
2019-02-22 18:18:07 +0800

    添加值传递测试

--- src/main/java/Others/base/StringDemo3.java
+++ src/main/java/Others/base/StringDemo3.java
@@ -5,6 +5,7 @@ import Utills.PrintUtill;
 /***
  * [Java调用函数传递参数到底是值传递还是引用传递](https://www.cnblogs.com/wutianqi/p/8723582.html)
  * [java之语法之方法调用之地址传值之空对象（null）](https://lixh1986.iteye.com/blog/2354853)
+ * [Java参数引用传递之例外：null](https://blog.csdn.net/stevesun13/article/details/51922169)
  */
 
 public class StringDemo3 {
