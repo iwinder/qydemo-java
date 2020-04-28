f4efeda5fabab74aab6a5ed38def86bd8c9fd3c5
windCoder <zhrucong@gmail.com>
2020-04-28 08:55:53 +0800

    调整单例模式

--- src/main/java/DesignPatterns/Creational/Singleton/geekbang/IdGenerator7.java
+++ src/main/java/DesignPatterns/Creational/Singleton/geekbang/IdGenerator7.java
@@ -13,6 +13,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 集群唯一
+ *
+ * [设计模式之美](https://windcoder.com/go/JKDesignPattern)中仅提供了伪代码，这里根据伪代码尝试做了一个基于Redis的简单实现，代码本身没做测试，请勿直接粘贴复制到生产环境。
+ *
+ * Redisson 操作redis实现单例对象的序列化存储与反序列化读取。
+ * Redisson 实现Redis分布式锁，用于取出前的加锁，与操作后的释放锁。
+ *
+ *  关于为何配置自定义序列化与反序列化的问题，可以参考[redisson如何序列化](https://www.php.cn/redis/436670.html)。
  */
 public class IdGenerator7 implements Serializable {
     private AtomicLong id = new AtomicLong(0);
@@ -20,6 +27,7 @@ public class IdGenerator7 implements Serializable {
   
 
     static RedissonClient redisson = getRedissonClient();
+    //  Redisson实现的Redis分布式锁锁
     static RLock  lock = redisson.getLock("anyLock");
 
 
@@ -50,10 +58,14 @@ public class IdGenerator7 implements Serializable {
     public long getId() {
         return id.incrementAndGet();
     }
-    
-    
+
+    /**
+     * 读取Redisson关于Redis的相关配置，生成Redisson对象。
+     *
+     * 这里仅为了方便实现与展示集群唯一的示例。
+     * @return
+     */
     private static RedissonClient getRedissonClient() {
-        //
         Config config = null;
         try {
             config = Config.fromYAML(new File("classpath:redisson.yaml"));
