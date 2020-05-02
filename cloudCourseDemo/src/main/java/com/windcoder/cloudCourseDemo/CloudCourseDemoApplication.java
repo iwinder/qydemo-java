fd8ed8acb20f9e948b350316589a9ea69a9c5d05
windCoder <zhrucong@gmail.com>
2020-05-03 00:22:02 +0800

    慕课网学习示例初始化

---
+++ cloudCourseDemo/src/main/java/com/windcoder/cloudCourseDemo/CloudCourseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.windcoder.cloudCourseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCourseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCourseDemoApplication.class, args);
+	}
+
+}
---
+++ cloudCourseDemo/src/test/java/com/windcoder/cloudCourseDemo/CloudCourseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windcoder.cloudCourseDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCourseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
