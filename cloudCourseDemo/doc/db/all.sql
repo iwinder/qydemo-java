4316d68eb3727e7a416b4ee53e13b147daa40e1a
windCoder <zhrucong@gmail.com>
2020-05-03 22:58:17 +0800

    增加大章Chapter表sql，生成持久层代码

--- cloudCourseDemo/doc/db/all.sql
+++ cloudCourseDemo/doc/db/all.sql
@@ -1,14 +1,20 @@
-SET FOREIGN_KEY_CHECKS=0;
+drop table if exists `chapter`;
+create table `chapter` (
+                           `id` char(8) not null comment 'ID',
+                           `course_id` char(8) comment '课程ID',
+                           `name` varchar(50) comment '名称',
+                           primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
 
-DROP TABLE IF EXISTS `test`;
-CREATE TABLE  test  (
-   id  varchar(255) NOT NULL,
-   name varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of test
--- ----------------------------
-INSERT INTO `test` VALUES (1, '测试');
-SET FOREIGN_KEY_CHECKS=1;
+-- -------------------- 测试
+
+drop table if exists `test`;
+create table `test` (
+                        `id` char(8) not null default '' comment 'id',
+                        `name` varchar(50) comment '名称',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `test` (id, name) values (1, '测试');
+insert into `test` (id, name) values (2, '测试2');
