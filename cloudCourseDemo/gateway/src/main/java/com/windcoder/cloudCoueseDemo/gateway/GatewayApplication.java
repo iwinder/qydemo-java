eb30ae8fab3925d6efcc2fbfb81bb009216f514b
windCoder <zhrucong@gmail.com>
2020-05-04 01:32:09 +0800

    1.解决gateway跨域问题，使用lb:/+注册中心名称做路由转发

--- cloudCourseDemo/gateway/src/main/java/com/windcoder/cloudCoueseDemo/gateway/GatewayApplication.java
+++ cloudCourseDemo/gateway/src/main/java/com/windcoder/cloudCoueseDemo/gateway/GatewayApplication.java
@@ -4,7 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  * 增加 @EnableEurekaClient 注解
@@ -24,4 +29,22 @@ public class GatewayApplication {
 
 	}
 
+	/**
+	 *  因为前端通过gateway访问，所以在这里配置允许跨域请求。
+	 * @return
+	 */
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(Boolean.TRUE);
+		config.addAllowedHeader("*");
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsWebFilter(source);
+	}
 }
--- cloudCourseDemo/server/src/main/java/com/windcoder/cloudCourseDemo/server/config/CorsConfig.java
+++ cloudCourseDemo/server/src/main/java/com/windcoder/cloudCourseDemo/server/config/CorsConfig.java
@@ -5,16 +5,16 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .maxAge(3600);// 1小时内不需要再预检（发OPTION请求）
-    }
-}
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                .maxAge(3600);// 1小时内不需要再预检（发OPTION请求）
+//    }
+//}
