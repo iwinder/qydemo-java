ec85c3a5d45ab4986f8824249ce8df2632e4cc1a
windCoder <zhrucong@gmail.com>
2020-05-03 23:38:18 +0800

    增加dto层，并修改chapter相关部分为dto实现

--- cloudCourseDemo/business/src/main/java/com/windcoder/cloudCourseDemo/business/controller/admin/ChapterController.java
+++ cloudCourseDemo/business/src/main/java/com/windcoder/cloudCourseDemo/business/controller/admin/ChapterController.java
@@ -1,21 +1,25 @@
-package com.windcoder.cloudCourseDemo.business.controller;
+package com.windcoder.cloudCourseDemo.business.controller.admin;
 
 import com.windcoder.cloudCourseDemo.server.domain.Chapter;
+import com.windcoder.cloudCourseDemo.server.dto.ChapterDto;
 import com.windcoder.cloudCourseDemo.server.service.ChapterService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
 
     @GetMapping("/chapter")
-    public List<Chapter> testA() {
+    public List<ChapterDto> testA() {
         return chapterService.list();
     }
 }
--- cloudCourseDemo/server/src/main/java/com/windcoder/cloudCourseDemo/server/service/ChapterService.java
+++ cloudCourseDemo/server/src/main/java/com/windcoder/cloudCourseDemo/server/service/ChapterService.java
@@ -2,10 +2,13 @@ package com.windcoder.cloudCourseDemo.server.service;
 
 import com.windcoder.cloudCourseDemo.server.domain.Chapter;
 import com.windcoder.cloudCourseDemo.server.domain.ChapterExample;
+import com.windcoder.cloudCourseDemo.server.dto.ChapterDto;
 import com.windcoder.cloudCourseDemo.server.mapper.ChapterMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,12 +16,19 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<Chapter> list() {
+    public List<ChapterDto> list() {
         ChapterExample testExample = new ChapterExample();
 //        testExample.setOrderByClause("id ASC");
-        testExample.createCriteria().andIdEqualTo("1");
-        testExample.setOrderByClause("id DESC");
-        return chapterMapper.selectByExample(testExample);
+//        testExample.createCriteria().andIdEqualTo("1");
+//        testExample.setOrderByClause("id DESC");
+        List<Chapter> chapters = chapterMapper.selectByExample(testExample);
+        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
+        for (Chapter chapter: chapters) {
+            ChapterDto chapterDto = new ChapterDto();
+            BeanUtils.copyProperties(chapter, chapterDto);
+            chapterDtoList.add(chapterDto);
+        }
+        return chapterDtoList;
     }
 
 }
