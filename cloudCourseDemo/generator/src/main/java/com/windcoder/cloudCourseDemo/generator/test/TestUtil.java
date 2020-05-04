b93fb400502c471bbcddb84f4095335227ba411b
windCoder <zhrucong@gmail.com>
2020-05-05 00:39:54 +0800

    增加generator模块,集成FreeMarker,用于代码生成

---
+++ cloudCourseDemo/generator/src/main/java/com/windcoder/cloudCourseDemo/generator/test/TestUtil.java
@@ -0,0 +1,28 @@
+package com.windcoder.cloudCourseDemo.generator.test;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestUtil {
+    static String filePath = "generator\\src\\main\\java\\com\\windcoder\\cloudCourseDemo\\generator\\test\\";
+    static String toPath = "generator\\src\\main\\java\\com\\windcoder\\cloudCourseDemo\\generator\\test\\";
+    public static void main(String[] args) throws IOException, TemplateException {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File(filePath));
+        cfg.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
+        Template temp = cfg.getTemplate("test.ftl");
+
+        FileWriter fw = new FileWriter(toPath+"Test.java");
+        BufferedWriter bw = new BufferedWriter(fw);
+        temp.process(null, bw);
+        bw.flush();
+        fw.close();
+    }
+}
