80fb88a6815f3074f4f6b8721419cff58022a571
windCoder <zhrucong@gmail.com>
2020-05-05 02:59:34 +0800

    生成器开发：代码生成器和mybatis generator整合

--- cloudCourseDemo/generator/src/main/java/com/windcoder/cloudCourseDemo/generator/server/ServerGenerator.java
+++ cloudCourseDemo/generator/src/main/java/com/windcoder/cloudCourseDemo/generator/server/ServerGenerator.java
@@ -4,7 +4,11 @@ import com.windcoder.cloudCourseDemo.generator.utils.DbUtil;
 import com.windcoder.cloudCourseDemo.generator.utils.Field;
 import com.windcoder.cloudCourseDemo.generator.utils.FreeMarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -17,12 +21,32 @@ public class ServerGenerator {
     static String toServicePath = "server\\src\\main\\java\\com\\windcoder\\cloudCourseDemo\\server\\service\\";
     static String toDtoePath = "server\\src\\main\\java\\com\\windcoder\\cloudCourseDemo\\server\\service\\";
     static String toControllerPath = MODULE + "\\src\\main\\java\\com\\windcoder\\cloudCourseDemo\\" + MODULE + "\\controller\\admin\\";
-
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
 
     public static void main(String[] args) throws Exception  {
-        String Domain = "Section";
-        String domain = "section";
-        String tableNameCn = "小节";
+        // 只生成配置文件中的第一个 table 节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader = new SAXReader();
+        // 读 取xml 文件到 Document 中
+        Document doc = reader.read(file);
+        // 读取 xml 文件的根节点
+        Element rootElement = doc.getRootElement();
+        // 读取 context 节点
+        Element contextElement = rootElement.element("context");
+        // 定义一个Element用于遍历
+        Element tableElement;
+        // 取第一个 table 的节点
+        tableElement = contextElement.elementIterator("table").next();
+
+
+        String Domain = tableElement.attributeValue("domainObjectName");
+        // 表名可能有下划线，所以不是小写的domain
+        String tableName =  tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0,1).toLowerCase() + Domain.substring(1);
+        System.out.println("表："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
+
         String module = MODULE;
         List<Field> fieldList = DbUtil.getColumnByTableName(domain);
         Set<String> typeSet = getJavaTypes(fieldList);
