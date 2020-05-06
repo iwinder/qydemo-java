975618d080c012a81237fe7cd7a499bb8e6c645f
windCoder <zhrucong@gmail.com>
2020-05-07 02:29:38 +0800

    文件上传功能开发：上传文件时保存文件记录

--- cloudCourseDemo/file/src/main/java/com/windcoder/cloudCourseDemo/file/controller/admin/UploadController.java
+++ cloudCourseDemo/file/src/main/java/com/windcoder/cloudCourseDemo/file/controller/admin/UploadController.java
@@ -1,11 +1,15 @@
 package com.windcoder.cloudCourseDemo.file.controller.admin;
 
+import com.windcoder.cloudCourseDemo.server.dto.FileDto;
 import com.windcoder.cloudCourseDemo.server.dto.ResponseDto;
+import com.windcoder.cloudCourseDemo.server.service.FileService;
 import com.windcoder.cloudCourseDemo.server.utils.UuidUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,20 +19,38 @@ import java.io.IOException;
 public class UploadController {
     public static final String BUSINESS_NAME = "文件上传";
 
+    @Value("${file.path}")
+    private String FILE_PATH;
+    @Resource
+    private FileService fileService;
+
     @PostMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
         log.info("上传文件开始：{}", file);
         log.info(file.getOriginalFilename());
         log.info(String.valueOf(file.getSize()));
 
+        // 保存文件到本地
         String fileName = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
-        String fullPath = "E:/00Work/Program/data/imooc/teacher/" + key + "-" + fileName;
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        String path = "teacher/" + key + "." + suffix;
+        String fullPath = FILE_PATH + path;
         File dest = new File(fullPath);
         file.transferTo(dest);
         log.info(dest.getAbsolutePath());
+
+        log.info("保存文件记录开始");
+        FileDto fileDto = new FileDto();
+        fileDto.setName(fileName);
+        fileDto.setPath(path);
+        fileDto.setSize(Math.toIntExact(file.getSize()));
+        fileDto.setSuffix(suffix);
+        fileDto.setUse("");
+        fileService.save(fileDto);
+
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setContent("/teacher/" + key + "-" + fileName);
+        responseDto.setContent(path);
         return responseDto;
     }
 
