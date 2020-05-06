3290b2248e94e0b2d51c068fa927aee0a75844ec
windCoder <zhrucong@gmail.com>
2020-05-07 04:36:12 +0800

    文件上传功能开发：增加课程内容文件管理，用于富文本框中插入图片或视频

--- cloudCourseDemo/admin/src/views/admin/course.vue
+++ cloudCourseDemo/admin/src/views/admin/course.vue
@@ -92,7 +92,7 @@
                             <div class="form-group">
                                 <label   class="col-sm-2 control-label">封面</label>
                                 <div class="col-sm-10"> 
-                                    <file v-bind:id="'image-upload'" v-bind:text="'上传封面'"  
+                                    <file v-bind:input-id="'image-upload'" v-bind:text="'上传封面'"  
                                             v-bind:suffixs="['jpg','jpeg','png']" 
                                             v-bind:use="FILE_USE.COURSE.key"
                                             v-bind:after-upload="afterUpload"
@@ -215,6 +215,36 @@
                         <h4 class="modal-title" id="myModalLabel">内容编辑</h4>
                     </div>  <!-- 课程内容Modal  header end-->
                     <div class="modal-body">
+                        <file v-bind:input-id="'content-file-upload'"
+                            v-bind:text="'上传文件1'"
+                            v-bind:suffixs="['jpg', 'jpeg', 'png', 'mp4']"
+                            v-bind:use="FILE_USE.COURSE.key"
+                            v-bind:after-upload="afterUploadContentFile"></file>
+                        <br>
+                        <table id="file-table" class="table  table-bordered table-hover">
+                            <thead>
+                            <tr>
+                            <th>名称</th>
+                            <th>地址</th>
+                            <th>大小</th>
+                            <th>操作</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <tr v-for="f in files"  v-bind:key="f.id">
+                            <td>{{f.name}}</td>
+                            <td>{{f.url}}</td>
+                            <td>{{f.size | formatFileSize}}</td>
+                            <td>
+                                <button v-on:click="delFile(f)" class="btn btn-white btn-xs btn-warning btn-round">
+                                <i class="ace-icon fa fa-times red2"></i>
+                                删除
+                                </button>
+                            </td>
+                            </tr>
+                            </tbody>
+                        </table>
                         <form class="form-horizontal">
                             <div class="form-group">
                                 <div class="col-lg-12">
@@ -238,31 +268,31 @@
 
 
         <!-- 排序Modal -->
-        <div id="course-sort-modal" class="modal fade" tabindex="-1" role="dialog">
+        <div id="course-sort-modal" class="modal fade" tabindex="-1" role="dialog" style="overflow:auto;">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title">排序</h4>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body"> 
                     <form class="form-horizontal">
-                    <div class="form-group">
-                        <label class="control-label col-lg-3">
-                        当前排序
-                        </label>
-                        <div class="col-lg-9">
-                        <input class="form-control" v-model="sort.oldSort" name="oldSort" disabled>
+                        <div class="form-group">
+                            <label class="control-label col-lg-3">
+                            当前排序
+                            </label>
+                            <div class="col-lg-9">
+                            <input class="form-control" v-model="sort.oldSort" name="oldSort" disabled>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-lg-3">
-                        新排序
-                        </label>
-                        <div class="col-lg-9">
-                        <input class="form-control" v-model="sort.newSort" name="newSort">
+                        <div class="form-group">
+                            <label class="control-label col-lg-3">
+                            新排序
+                            </label>
+                            <div class="col-lg-9">
+                            <input class="form-control" v-model="sort.newSort" name="newSort">
+                            </div>
                         </div>
-                    </div>
                     </form>
                 </div>
                 <div class="modal-footer">
@@ -307,6 +337,7 @@
                     newSort: 0,
                 },
                 teachers: [],
+                files: [],
                 
             }
         },
@@ -490,6 +521,8 @@
                 // 先清空历史文本
                 $("#content").summernote('code', '');
                 _this.saveContentLabel = "";
+                 // 加载内容文件列表
+                _this.listContentFiles();
                 Loading.show();
                 _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course/find-content/' + id).then((response)=>{
                     Loading.hide();
@@ -586,6 +619,53 @@
                 let image = response.content.path;
                 _this.course.image = process.env.VUE_APP_FILE_SERVER + image;
            },
+            /**
+             * 加载内容文件列表
+             */
+            listContentFiles() {
+                let _this = this;
+                _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course-content-file/list/' + _this.course.id).then((response)=>{
+                    let resp = response.data;
+                    if (resp.success) {
+                        _this.files = resp.content;
+                    }
+                });
+            },
+
+            /**
+             * 上传内容文件后，保存内容文件记录
+             */
+            afterUploadContentFile(response) {
+                let _this = this;
+                console.log("开始保存文件记录");
+                let file = response.content;
+                file.courseId = _this.course.id;
+                file.url = file.path;
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course-content-file/save', file).then((response)=>{
+                let resp = response.data;
+                if (resp.success) {
+                    Toast.success("上传文件成功");
+                    _this.files.push(resp.content);
+                }
+                });
+
+            },
+
+            /**
+             * 删除内容文件
+             */
+            delFile(f) {
+                let _this = this;
+                Confirm.show("删除课程后不可恢复，确认删除？", function () {
+                _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/course-content-file/delete/' + f.id).then((response)=>{
+                    let resp = response.data;
+                    if (resp.success) {
+                    Toast.success("删除文件成功");
+                    Tool.removeObj(_this.files, f);
+                    }
+                });
+                });
+            },
         }
     }
 </script>
