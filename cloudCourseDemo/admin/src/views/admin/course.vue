ba9204c47a9319f155233771cd6da749bcd01285
windCoder <zhrucong@gmail.com>
2020-05-06 16:10:30 +0800

    增加自动保存

--- cloudCourseDemo/admin/src/views/admin/course.vue
+++ cloudCourseDemo/admin/src/views/admin/course.vue
@@ -175,6 +175,11 @@
                         <form class="form-horizontal">
                             <div class="form-group">
                                 <div class="col-lg-12">
+                                    {{saveContentLabel}}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-lg-12">
                                     <div id="content"></div>
                                 </div>
                             </div>
@@ -200,14 +205,15 @@
         components: {Pagination},
         data: function() {
             return {
-            course: {},
-            courses: [],
-            COURSE_LEVEL:COURSE_LEVEL,
-            COURSE_CHARGE:COURSE_CHARGE,
-            COURSE_STATUS:COURSE_STATUS,
-            categorys: {},
-            tree: {},
-        }
+                course: {},
+                courses: [],
+                COURSE_LEVEL:COURSE_LEVEL,
+                COURSE_CHARGE:COURSE_CHARGE,
+                COURSE_STATUS:COURSE_STATUS,
+                categorys: {},
+                tree: {},
+                saveContentLabel:'',
+            }
         },
         mounted: function() {
             // this.$parent.activeSidebar("sidebar-business-course");
@@ -383,7 +389,9 @@
                     focus: true,
                     height: 300
                 });
+                // 先清空历史文本
                 $("#content").summernote('code', '');
+                _this.saveContentLabel = "";
                 Loding.show();
                 _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course/find-content/' + id).then((response)=>{
                     Loding.hide();
@@ -394,6 +402,14 @@
                         if(resp.content) {
                             $("#content").summernote('code', resp.content.content);
                         }
+                        // 定时自动保存
+                        let saveContentInterval = setInterval(function() {
+                            _this.saveContent();
+                        }, 5000);
+                        // 关闭内容框时，清空自动保存任务
+                        $('#course-content-modal').on('hidden.bs.modal', function (e) {
+                            clearInterval(saveContentInterval);
+                        })
                     } else {
                          Toast.warning(resp.message);
                     }
@@ -409,7 +425,9 @@
                     Loding.hide();
                     let resp = response.data;
                     if (resp.success) {
-                        Toast.success("内容保存成功");
+                        // Toast.success("内容保存成功");
+                         let now = Tool.dateFormat("hh:mm:ss");
+                        _this.saveContentLabel = "最后保存时间：" + now;
                     } else {
                         Toast.warning(resp.message);
                     }
