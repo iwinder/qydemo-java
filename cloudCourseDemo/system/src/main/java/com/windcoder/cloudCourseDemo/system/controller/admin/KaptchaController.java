fa2daa51c4f552a49bf5531ea222d799ec3eb4c6
windCoder <zhrucong@gmail.com>
2020-05-09 01:24:49 +0800

    用户管理与登录：集成图形验证码kaptcha

---
+++ cloudCourseDemo/system/src/main/java/com/windcoder/cloudCourseDemo/system/controller/admin/KaptchaController.java
@@ -0,0 +1,59 @@
+package com.windcoder.cloudCourseDemo.system.controller.admin;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/admin/kaptcha")
+public class KaptchaController {
+    @Qualifier("getDefaultKaptcha")
+    @Autowired
+    DefaultKaptcha defaultKaptcha;
+
+    @GetMapping("/image-code/{imageCodeToken}")
+    public void imageCode(@PathVariable(value = "imageCodeToken") String imageCodeToken,
+                          HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ByteArrayOutputStream jpegOutputStrem = new ByteArrayOutputStream();
+
+
+        try {
+            // 生成验证码字符串
+            String createText = defaultKaptcha.createText();
+
+            // 将生成的验证码放入会话缓存中，后续验证的时候使用
+            request.getSession().setAttribute(imageCodeToken, createText);
+
+            // 使用验证码字符串生成验证码图片
+            BufferedImage challenge = defaultKaptcha.createImage(createText);
+            ImageIO.write(challenge, "jpg", jpegOutputStrem);
+        } catch (IOException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 定义response输出类型为image/jpeg类型，使用response输出图片的byte数组
+        byte[] captchaChallengeAsJpeg = jpegOutputStrem.toByteArray();
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream  = response.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+
+    }
+}
