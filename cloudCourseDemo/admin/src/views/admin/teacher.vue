52e3297ea712e3d6e043f0779a5e18f9924c52ff
windCoder <zhrucong@gmail.com>
2020-05-12 01:37:59 +0800

    修改讲师头像上传组件

--- cloudCourseDemo/admin/src/views/admin/teacher.vue
+++ cloudCourseDemo/admin/src/views/admin/teacher.vue
@@ -95,11 +95,11 @@
                             <div class="form-group">
                                 <label   class="col-sm-2 control-label">头像</label>
                                 <div class="col-sm-10"> 
-                                    <oss-file v-bind:input-id="'image-upload'" v-bind:text="'上传头像'"  
+                                    <big-file v-bind:input-id="'image-upload'" v-bind:text="'上传头像'"  
                                             v-bind:suffixs="['jpg','jpeg','png']" 
                                             v-bind:use="FILE_USE.TEACHER.key"
                                             v-bind:after-upload="afterUpload"
-                                            ></oss-file>
+                                            ></big-file>
                                      <div v-show="teacher.image" class="row">
                                             <div class="col-md-4">
                                                 <img v-bind:src="teacher.image" class="img-responsive">
@@ -147,11 +147,11 @@
 <script>
 
     import Pagination from '../../components/pagination';
-    // import BigFile from '../../components/big-file';
-    import OssFile from '../../components/oss-file';
+    import BigFile from '../../components/big-file';
+    // import OssFile from '../../components/oss-file';
     export default {
         name: 'business-teacher',
-        components: {Pagination,OssFile},
+        components: {Pagination,BigFile},
         data: function() {
             return {
                 teacher: {},
