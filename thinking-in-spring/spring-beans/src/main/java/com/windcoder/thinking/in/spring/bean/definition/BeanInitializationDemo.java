886d5bb318946e1fd4bc5bc068436bd8d4c1a47c
windCoder <zhrucong@gmail.com>
2020-05-13 14:09:23 +0800

    BeanDefinition 初始化方式

--- thinking-in-spring/spring-beans/src/main/java/com/windcoder/thinking/in/spring/bean/definition/BeanInitializationDemo.java
+++ thinking-in-spring/spring-beans/src/main/java/com/windcoder/thinking/in/spring/bean/definition/BeanInitializationDemo.java
@@ -1,4 +1,35 @@
 package com.windcoder.thinking.in.spring.bean.definition;
 
+import com.windcoder.thinking.in.spring.bean.definition.factory.abstractFactory.DefaultUserFactory;
+import com.windcoder.thinking.in.spring.bean.definition.factory.abstractFactory.IUserFactory;
+import com.windcoder.thinking.in.spring.common.utils.PrintZUtill;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 默认 {@link com.windcoder.thinking.in.spring.bean.definition.factory.UserFacotry} 实现
+ */
 public class BeanInitializationDemo {
+
+    public static void main(String[] args) {
+        // 创建BeanFactory容器
+        AnnotationConfigApplicationContext applicationContext  = new AnnotationConfigApplicationContext();
+        // 注册 Configuration Class(配置类)
+        //  当 Class 被 register 之后，标注 @Configuration 与否都不在重要
+        applicationContext.register(BeanInitializationDemo.class);
+
+        // 启动Spring 应用上下文
+        applicationContext.refresh();
+
+        // 依赖查找
+        IUserFactory userFactory = applicationContext.getBean(IUserFactory.class);
+        PrintZUtill.println(userFactory.createUser());
+        // 关闭 Spring 应用上下文
+        applicationContext.close();
+    }
+
+    @Bean(initMethod = "initUserFactory")
+    public IUserFactory userFactory() {
+        return new DefaultUserFactory();
+    }
 }
--- thinking-in-spring/spring-beans/src/main/java/com/windcoder/thinking/in/spring/bean/definition/factory/abstractFactory/DefaultUserFactory.java
+++ thinking-in-spring/spring-beans/src/main/java/com/windcoder/thinking/in/spring/bean/definition/factory/abstractFactory/DefaultUserFactory.java
@@ -1,7 +1,28 @@
 package com.windcoder.thinking.in.spring.bean.definition.factory.abstractFactory;
 
+import com.windcoder.thinking.in.spring.common.utils.PrintZUtill;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+
 /**
  *  {@link IUserFactory} 抽象工厂的默认实现
  */
-public class DefaultUserFactory implements IUserFactory {
+public class DefaultUserFactory implements IUserFactory, InitializingBean {
+
+    // 基于@PostConstruct 注解
+    // 1.6已有的Java标准注解
+    @PostConstruct
+    public void init() {
+        PrintZUtill.println("@PostConstruct: IUserFactory 初始化中");
+    }
+
+    public void initUserFactory() {
+        PrintZUtill.println("自定义初始化方法 initUserFactory: IUserFactory 初始化中");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        PrintZUtill.println("InitializingBean#afterPropertiesSet: IUserFactory 初始化中");
+    }
 }
