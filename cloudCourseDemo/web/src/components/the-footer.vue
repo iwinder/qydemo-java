63439b4b839e24e263342341cfdbab3d8d0522c7
windCoder <zhrucong@gmail.com>
2020-05-18 11:20:51 +0800

    网站首页开发，将顶部提取成the-header组件,将底部提取成the-footer组件

---
+++ cloudCourseDemo/web/src/components/the-footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Back to top</a>
+      </p>
+      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+      <p>New to Bootstrap? <a href="https://v4.bootcss.com/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'theFooter',
+  props: {
+     
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
